6dc2a1aeafb7e8dee7c0a080aca852aac83a6e61
Molly Fei <feimolly365@gmail.com>
2025-10-17 13:23:35 -0400

    Modify users.sql to include comments and drop table
    Added comments and a drop statement for the users table.

--- db/users.sql
+++ db/users.sql
@@ -1,4 +1,8 @@
-CREATE TABLE IF NOT EXISTS users (
+-- Users Service Database Schema
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
